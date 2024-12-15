cfbe6e1de43469f77c593fc86d8f63b4226e5074
Dustin Lodge <lodge.dustin@outlook.com>
2024-12-14 22:42:52 -0700

    updated var

--- src/js/weather_api.mjs
+++ src/js/weather_api.mjs
@@ -1,5 +1,5 @@
 const apiKey = import.meta.env.VITE_WEATHER_API_KEY; 
-const url = 'http://api.openweathermap.org/data/2.5/weather'; 
+const url = 'https://api.openweathermap.org/data/2.5/weather'; 
 // 
 export async function getWeather(city) { 
     try { 
