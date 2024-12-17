b1cf47c6f3541c37c21f4d4adffb3d4656e147c6
Dustin Lodge <lodge.dustin@outlook.com>
2024-12-17 10:55:38 -0700

    cleaned up

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
-import { getCurrentWeather, displayCurrentWeather } from '/js/currentWeather.mjs';
-import { getForecastWeather, displayForecastWeather } from '/js/forecastWeather.mjs';
+import { getCurrentWeather, displayCurrentWeather } from "./currentWeather.mjs";
+import {
+  getForecastWeather,
+  displayForecastWeather,
+} from "./forecastWeather.mjs";
 
-const cityInput = document.querySelector('.cityInput');
-const weatherButton = document.querySelector('.weatherButton');
-const weatherData = document.querySelector('.weatherData');
+const cityInput = document.querySelector(".cityInput");
+const weatherButton = document.querySelector(".weatherButton");
+const weatherData = document.querySelector(".weatherData");
 
-weatherButton.addEventListener('click', async () => {
+weatherButton.addEventListener("click", async () => {
   const city = cityInput.value;
   if (city) {
     try {
@@ -18,17 +21,15 @@ weatherButton.addEventListener('click', async () => {
       `;
       // catch 404 error?
     } catch (error) {
-      weatherData.innerHTML = '<p>Error fetching weather data.</p>';
+      weatherData.innerHTML = "<p>Error fetching weather data.</p>";
     }
   }
 });
 
-
-
 /*
 function displayWeather( forecast) {
-  const dailyForecast = forecast.list.filter(item => item.dt_txt.includes('12:00:00')); // Filter data for 12:00 PM each day
-  weatherData.innerHTML = '<h2>Daily Weather Forecast</h2>';
+  const dailyForecast = forecast.list.filter(item => item.dt_txt.includes("12:00:00")); // Filter data for 12:00 PM each day
+  weatherData.innerHTML = "<h2>Daily Weather Forecast</h2>";
   dailyForecast.forEach(day => {
     const date = new Date(day.dt * 1000).toLocaleDateString();
     const temp = Math.round(day.main.temp);
@@ -62,9 +63,6 @@ function displayWeather( forecast) {
     <p>${JSON.stringify(forecast.city.coord)}</p>
   `;
 }*/
-
-
-
 
 /*
 
