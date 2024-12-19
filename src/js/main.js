07713f5b927f01064caef5d165e53dc8a2a7eadf
Dustin Lodge <lodge.dustin@outlook.com>
2024-12-18 22:46:18 -0700

    added dropdown event and final touches

--- src/js/main.js
+++ src/js/main.js
@@ -9,34 +9,34 @@ const weatherButton = document.querySelector(".weatherButton");
 const weatherData = document.querySelector(".weatherData");
 const wrapper3 = document.querySelector(".wrapper-3");
 
-
 // Utah cities API
-const ut_cities_api = "https://services1.arcgis.com/99lidPhWCzftIe9K/arcgis/rest/services/CitiesTownsLocations/FeatureServer/0/query?where=1%3D1&outFields=NAME,COUNTY&outSR=4326&f=json"
+const ut_cities_api =
+  "https://services1.arcgis.com/99lidPhWCzftIe9K/arcgis/rest/services/CitiesTownsLocations/FeatureServer/0/query?where=1%3D1&outFields=NAME,COUNTY&outSR=4326&f=json";
 
 // get Utah cities
 fetch(ut_cities_api)
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     // LOOP and SORT city names
-    const cityNames = data.features.map(feature => feature.attributes.NAME);
+    const cityNames = data.features.map((feature) => feature.attributes.NAME);
     cityNames.sort();
 
     // List each city name in dropdown
-    cityNames.forEach(name => {
+    cityNames.forEach((name) => {
       const option = document.createElement("option");
       option.value = name;
       // Learned this is required to display city names. Are blank without it.
       option.textContent = name;
       cityInput.appendChild(option);
-    })
-      const option = document.createElement("option");
-      option.value = feature.attributes.NAME;
-      option.textContent = feature.attributes.NAME;
-      cityInput.appendChild(option);
-    })
-  .catch(error => error);
+    });
+    const option = document.createElement("option");
+    option.value = feature.attributes.NAME;
+    option.textContent = feature.attributes.NAME;
+    cityInput.appendChild(option);
+  })
+  .catch((error) => error);
 
-  // EVENT listener
+// EVENT listener
 weatherButton.addEventListener("click", async () => {
   const city = cityInput.value;
   if (city) {
@@ -50,12 +50,25 @@ weatherButton.addEventListener("click", async () => {
       wrapper3.innerHTML = `
       ${displayForecastWeather(forecastWeather)}<br>
       `;
-      
+
       // catch 404 error?
     } catch (error) {
-      weatherData.innerHTML = "<p id='validation'>*** Error fetching weather data ***</p>";
+      weatherData.innerHTML =
+        "<p id='validation'>*** Error fetching weather data ***</p>";
     }
   } else {
-    weatherData.innerHTML = "<p id='validation'>*** Please select a city ***</p>";
+    weatherData.innerHTML =
+      "<p id='validation'>*** Please select a city ***</p>";
   }
+});
+
+// EVENT to enhance the dropdown list view
+cityInput.addEventListener("focus", () => {
+  cityInput.style.width = "200px";
+  cityInput.style.height = "50px";
+});
+
+cityInput.addEventListener("blur", () => {
+  cityInput.style.width = "100px";
+  cityInput.style.height = "40px";
 });
